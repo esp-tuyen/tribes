b672f0cefddcf2d6d5970a2b720870b8cd3552bb
tuyen <tuyen.tran@eastplayers.io>
2022-10-04 14:45:46 +0700

    setup navigation && folder && components Text && icon svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigation from '~navigation/AppNavigation';
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.background}>
-        <Text style={styles.text}>Hello world</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <AppNavigation />
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  background: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-  },
-  text: {
-    fontFamily: 'Cascadia Code',
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'red',
-  },
-});
 
 export default App;
---
+++ src/components/TextCustom/TextCustomt.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Cascadia Code',
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '400',
+  },
+});
+
+export default styles;
